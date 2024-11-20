73b3156d1e9af665547eac9a71536fcb41cffe81
Max <uqwfa@student.kit.edu>
2024-11-20 20:30:23 +0100

    einfach krass

---
+++ catkin_ws/src/learn_enviroment/src/learn_env.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import sys
+import rospy
+
+
+if __name__ == '__main__':
+    print('start')
+    rospy.init_node('learn_env')
+    rospy.loginfo('initialize')
---
+++ catkin_ws/src/learn_enviroment/src/rviz_panel.cpp
@@ -0,0 +1,39 @@
+#include "learn-enviroment/rviz_panel.hpp"
+#include <pluginlib/class_list_macros.hpp>
+#include <QProcess>
+#include <QFile>
+#include <QDebug>
+#include <ros/ros.h>
+
+
+Learn_Window::Learn_Window(QWidget *parent)
+    : rviz::Panel(parent), ui(new Ui::Learn_Window), process(new QProcess(this)) {
+        ui ->setupUi(this);
+        connect(ui->executeButton, SIGNAL(clicked()), this, SLOT(onExecuteButtonClicked()));
+    }
+
+Learn_Window::~Learn_Window() {
+    delete ui;
+}
+
+void Learn_Window::load(const rviz::Config &config) {
+    rviz::Panel::load(config);
+}
+
+void Learn_Window::save(rviz::Config config) const {
+    rviz::Panel::save(config);
+}
+
+void Learn_Window::onExecuteButtonClicked() {
+    QString scriptPath = "/catkin_ws/src/learn_enviroment/src/learn_env.py";
+
+    if (!QFile::exists(scriptPath)) {
+        ROS_INFO("File not found: %s", scriptPath.toStdString().c_str());
+        return;
+    }
+
+    ROS_INFO("Starting script: %s", scriptPath.toStdString().c_str());
+    process->start("python3", QStringList() << scriptPath);
+}
+
+PLUGINLIB_EXPORT_CLASS(Learn_Window, rviz::Panel)
